852f4a72e749575ea0afa4ab65e01e280e039648
Joseph Rock <josephrock225@gmail.com>
2021-11-22 13:53:27 -0800

    More refactoring/cleanup

--- hangman.py
+++ hangman.py
@@ -13,6 +13,7 @@ class Game:
         self.is_cheating = False
         self.game_board = ["__" for i in range(self.word_length)]
 
+    # TODO fix logic
     def guess(self, letter):
         # if correct
         if letter in self.word:
@@ -33,14 +34,10 @@ class Game:
         self.lives += num
 
 
-def read_words() -> list:
+def get_random_word() -> str:
     with open("./words.txt", "r") as f:
         word_list = f.read().splitlines()
 
-    return word_list
-
-
-def get_random_word() -> str:
     seed()
     random_index = randint(0, len(word_list))
     return word_list[random_index]
@@ -53,13 +50,7 @@ def clear_screen():
         _ = system('clear')
 
 
-def init_board(word_length):
-    game_board = ["__" for i in range(word_length)]
-    return game_board
-
-
 def print_screen(game):
-    print()
     print(*game.game_board, ' ')
     print()
     print()
@@ -71,21 +62,17 @@ def print_screen(game):
     print("Guess letter: ", end="")
 
 
-# See above
 def print_win(lives):
-    print()
-    print()
-    print()
     print("Congrats! You survived with", lives, "lives remaining!")
     print("Press ENTER to start again")
+    print()
     input()
 
 
-# See above above
 def print_lose(guess_word):
-    print()
     print("Game OVER!, the word was", guess_word.upper())
     print("Press ENTER to start again")
+    print()
     input()
 
 
@@ -103,33 +90,34 @@ def quit_game():
     exit()
 
 
-# TODO fix logic
-def usr_input():
+def usr_input(game):
     while True:
-        keywords = ["help", "quit", "lives", "cheat"]
         guess = input()
 
-        if guess in keywords:
-            return guess
+        if guess == "lives":
+            game.add_lives(6)
+            break
+        elif guess == "cheat":
+            game.cheat()
+            break
+        elif guess == "quit":
+            quit_game()
+        elif guess == "help":
+            print_help()
         elif len(guess) != 1 or guess.isalpha() == False:
             print("Not valid input")
         else:
-            return guess
+            game.guess(guess)
+            break
 
 
-# Main
 def main():
     while True:
-        # inits 
-        remaining_guess = 0
         game = Game(get_random_word())
 
-        # Actual game loop
         while True:
             clear_screen()
-            print_screen(game)
-
-            answer_wrong = True      
+            print_screen(game)    
 
             if "__" not in game.game_board:
                 print_win(game.lives)
@@ -138,26 +126,6 @@ def main():
                 print_lose(game.word)
                 break
 
-            guess = usr_input()
+            usr_input(game)
 
-            if guess == "lives":
-                game.add_lives(6)
-            elif guess == "cheat":
-                game.cheat()
-            elif guess == "quit":
-                quit_game()
-            elif guess == "help":
-                print_help()
-            else:
-                # Check if answer correct and update board
-                game.guess(guess)
-
-word_list = read_words()
 main()
-
-game = Game(get_random_word())
-print(game.guesses)
-print(game.word)
-print(game.word_length)
-game.guess("A")
-print(game.guesses)
