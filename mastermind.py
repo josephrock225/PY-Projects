59d2261ab1b9d2662c1182e3047fdff976c7ab54
Joseph Rock <josephrock225@gmail.com>
2021-11-24 10:50:01 -0800

    Final bit of cleanup

--- mastermind.py
+++ mastermind.py
@@ -33,7 +33,7 @@ def select_gamemode(game) -> list:
     while True:
         print("Select your gamemode.")
         print("Press 1 for no repeating colors.")
-        print("Press 2 for repeating colors.")
+        print("Press 2 to allow repeating colors.")
         choice = input()
 
         if choice == "1":
@@ -46,7 +46,6 @@ def select_gamemode(game) -> list:
 
 
 def get_input(game) -> list:
-    # check for correct colors
     def in_colors(guess):
         for i in guess:
             if i not in game.colors:
@@ -56,7 +55,7 @@ def get_input(game) -> list:
     while True:
         guess = input().lower().replace(' ', '')
 
-        # too many letters
+        # too many char
         if len(guess) != 4:
             continue
 
@@ -66,6 +65,27 @@ def get_input(game) -> list:
 
         game.guess = [i for i in guess]
         break
+
+
+def check_ans(game):
+    game.black_peg = 0
+    game.white_peg = 0
+    ans_copy = game.answer.copy()
+    guess_copy = game.guess.copy()
+
+    # check for correct color correct spot
+    for index, ans_letter in enumerate(ans_copy):
+        if ans_letter == guess_copy[index]:
+            ans_copy.pop(index)
+            ans_copy.insert(index, "")
+            guess_copy.pop(index)
+            guess_copy.insert(index, "")
+            game.black_peg += 1
+
+    # check for correct color incorrect spot
+    for i in guess_copy:
+        if i != "" and i in ans_copy:
+            game.white_peg += 1
 
 
 def draw_screen(game):
@@ -79,7 +99,6 @@ def draw_screen(game):
 
     print("Guess 4 colors: ")
     print("Colors:", *game.colors)
-    print(game.answer)
 
 
 def update_board(game):
@@ -93,26 +112,6 @@ def update_board(game):
     game.key_board[game.round].insert(1, game.white_peg)
 
 
-def check_ans(game):
-    game.black_peg = 0
-    game.white_peg = 0
-    ans_copy = game.answer.copy()
-    guess_copy = game.guess.copy()
-
-    # check for correct spot
-    for index, ans_letter in enumerate(ans_copy):
-        if ans_letter == guess_copy[index]:
-            ans_copy.pop(index)
-            ans_copy.insert(index, "")
-            guess_copy.pop(index)
-            guess_copy.insert(index, "")
-            game.black_peg += 1
-
-    for i in guess_copy:
-        if i != "" and i in ans_copy:
-            game.white_peg += 1
-
-
 def main():
     while True:
         clear_screen()
@@ -123,7 +122,6 @@ def main():
             clear_screen()
             draw_screen(game)
 
-            #TODO check for win/loss condition
             if game.black_peg == 4:
                 print("Winner!")
                 break
@@ -133,34 +131,13 @@ def main():
                 print("The answer was:", *game.answer)
                 break
             
-            # get input
             get_input(game)
-            
-            # check ans
             check_ans(game)
-
-            #TODO update board
             update_board(game)
-            
             game.round -= 1
 
+        print("Press any key to play again.")
         input()
 
 
 main()
-
-# game = Game()
-# game.answer = ['w', 'g', 'b', 'p']
-# game.guess = ['w', 'w', 'w', 'w']
-# check_ans(game)
-# print("Black:", game.black_peg, "White", game.white_peg)
-
-# game.answer = ['r', 'r', 'r', 'p']
-# game.guess = ['p', 'w', 'r', 'r']
-# check_ans(game)
-# print("Black:", game.black_peg, "White", game.white_peg)
-
-# seed()
-# colors = ["r", "g", "b", "p", "y", "w"]
-
-# print(choices(colors, k=4))
